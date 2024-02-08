d436520452d15e872e9daceb4d7ba9679bd4e929
sanyath007 <sanyath007@gmail.com>
2024-02-08 12:00:28 +0700

    feat: create new Navbar component and add its to DefaultLayout

--- src/views/DefaultLayout.jsx
+++ src/views/DefaultLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 const DefaultLayout = () => {
     return (
         <div>
+            <Navbar />
 
             <section className="flex justify-center min-h-[680px]">
                 <Outlet />
