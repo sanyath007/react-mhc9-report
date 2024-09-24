61c94ac80cf167ba6ab0bc67977347d75cd74d84
sanyath007 <sanyath007@gmail.com>
2024-09-24 08:13:58 +0700

    feat: create new AuthContext file with AuthProvider and add AuthProvider to wrap App in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
 import store from './features/store.js'
+import { AuthProvider } from './contexts/AuthContext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <Router>
-                <App />
-            </Router>
+            <AuthProvider>
+                <Router>
+                    <App />
+                </Router>
+            </AuthProvider>
         </Provider>
     </React.StrictMode>,
 )
